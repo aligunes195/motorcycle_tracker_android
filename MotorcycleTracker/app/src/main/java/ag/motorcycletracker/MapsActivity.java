2342edbf3c673ed4eca44951a045adcec2dc3cd4
Ali Gunes <ali.gunes195@gmail.com>
2018-05-24 22:21:43 +0300

    HttpResponse reading added

--- MotorcycleTracker/app/src/main/java/ag/motorcycletracker/MapsActivity.java
+++ MotorcycleTracker/app/src/main/java/ag/motorcycletracker/MapsActivity.java
@@ -1,5 +1,7 @@
 package ag.motorcycletracker;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,10 +15,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final static String webpage = "https://project-bff.eu-gb.mybluemix.net/bff";
 
     private GoogleMap mMap;
     private double lat;
@@ -55,8 +67,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void updateLocation() {
         if (!isMapReady)
             return;
-        lat += 0.0001;
-        lng += 0.0001;
+        new GetLatLngTask().execute();
         Log.d("tagA", "Update latlng : " + lat + ", " + lng);
         runOnUiThread(new Runnable() {
             @Override
@@ -64,5 +75,65 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 marker.setPosition(new LatLng(lat, lng));
             }
         });
+    }
+
+    public String getHttpData() {
+        BufferedReader in = null;
+        String data = "";
+        try
+        {
+            HttpClient client = new DefaultHttpClient();
+            URI website = new URI(webpage);
+            HttpGet request = new HttpGet();
+            request.setURI(website);
+            HttpResponse response = client.execute(request);
+            response.getStatusLine().getStatusCode();
+
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String l = "";
+            String nl = System.getProperty("line.separator");
+            while ((l = in.readLine()) !=null){
+                sb.append(l + nl);
+            }
+            in.close();
+            data = sb.toString();
+        }
+        catch (Exception e)
+        {
+            Log.d("HttpException", e.getMessage());
+        }
+        finally
+        {
+            if (in != null){
+                try{
+                    in.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return data;
+    }
+
+    private class GetLatLngTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            return getHttpData();
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onPostExecute(final String data) {
+            // Burası düzenlenmeli !!!
+            lat += 0.0001;
+            lng += 0.0001;
+            Log.d("Data", data);
+        }
     }
 }
